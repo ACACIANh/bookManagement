6d05ed0bb62dddc4b3d27fa223036efed013bb34
DESKTOP-1FENOAP\ACACIAN_L14 <gkehdrn95@naver.com>
2021-06-16 10:09:20 +0900

    이클립스로 진행하기로 결정

--- src/xyz/acacian/objects/Book.java
+++ src/xyz/acacian/objects/Book.java
@@ -6,9 +6,11 @@ import java.util.Objects;
 import xyz.acacian.enums.ECategory;
 
 public class Book implements Serializable {
+
+	private static final long serialVersionUID = -7961318480811635722L;
+
 	public static String[] expressAttribute = {"고유 번호", "제목", "저자", "출판사"};
 	
-	private static final long serialVersionUIDzz = 1060401278850448591L;
 	private static int seedId = 0;
 
 	private int id = 0;
